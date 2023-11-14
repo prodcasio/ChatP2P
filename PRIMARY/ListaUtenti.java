3c599ecc36d2ff27341a199ea0604eae5876965b
Pierluigi Crotti <antonaccimattia05@gmail.com>
2023-11-14 09:22:18 +0100

    UPLOADED FILES

---
+++ PRIMARY/ListaUtenti.java
@@ -0,0 +1,16 @@
+package chatbeta1.pkg0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ListaUtenti {
+    public ArrayList<String> lista = new ArrayList<>();
+}
---
+++ PRIMARY/Retriever.java
@@ -0,0 +1,39 @@
+package chatbeta1.pkg0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Listener extends Thread {
+
+    Land() {
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                ServerSocket ss = new ServerSocket(1235);
+                System.out.println("Sto ascoltando la richiesta");
+                Socket s = ss.accept();
+                System.out.println("Socket accettato.");
+                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String request = br.readLine();
+                String ipReceived = request.split("/")[0];
+                System.out.println(request);
+                ss.close();
+                s.close();
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Land.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
